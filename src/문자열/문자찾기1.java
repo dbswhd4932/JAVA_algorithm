4392fb67c2c244900126ea216349276b840834ba
yoonjong <dbswhd4932@gmail.com>
2022-07-11 01:00:17 +0900

    22.07.11

---
+++ src/문자열/문자찾기1.java
@@ -0,0 +1,36 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 문자찾기1 {
+    public static int solution(String str, char t) {
+        int answer = 0;
+
+        str = str.toUpperCase();
+        t = Character.toUpperCase(t);
+
+        char[] chars = str.toCharArray();
+        for (char x : chars) {
+            if(x == t ) {
+                answer++;
+            }
+        }
+
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+        System.out.println(solution(str, c));
+
+    }
+}
+
