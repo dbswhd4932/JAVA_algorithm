9c0dfa5a7e886c944b0a444325332098ba8d84bf
yoonjong <dbswhd4932@gmail.com>
2022-07-14 00:24:43 +0900

    22.07.14 RE

---
+++ src/문자열/문자열압축11X.java
@@ -0,0 +1,30 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 문자열압축11X {
+    static String solution(String str) {
+        String answer = "";
+        int count = 1;
+        str += " "; //빈문자열을 넣어줘야 맨 마지막값 구할 수 있음.
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                answer += str.charAt(i);
+                if (count >= 2) {
+                    answer += count;
+                    count = 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+}
