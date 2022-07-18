4b7cee23bc30c0dbd28a0a6b315439ec5ed84ecd
yoonjong <dbswhd4932@gmail.com>
2022-07-18 23:44:02 +0900

    22.07.18

---
+++ src/배열1_2차원/봉우리10_2.java
@@ -0,0 +1,40 @@
+package 배열1_2차원;
+
+import java.util.Scanner;
+
+public class 봉우리10_2 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    static int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if(nx >=0 && nx < n && ny >= 0 && ny < n && arr[i][j] <= arr[nx][ny]){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) answer ++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(solution(n, arr));
+
+    }
+}
---
+++ src/효율성/두배열합치기.java
@@ -0,0 +1,39 @@
+package 효율성;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 두배열합치기 {
+    static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            answer.add(a[i]);
+        }
+        for ( int i = 0; i < m; i++) {
+            answer.add(b[i]);
+        }
+
+        Collections.sort(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+        for (int x : solution(n, m, a, b)) {
+            System.out.print(x + " ");
+        }
+    }
+}
